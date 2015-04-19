73cc368d6044abffb3f92d907ba61cb38ef6cc76
Hunter Hillegas <hunter@lastonepicked.com>
2015-04-19 12:02:06 -0700

    mailer for tournament upcoming

--- app/controllers/crontab_controller.rb
+++ app/controllers/crontab_controller.rb
@@ -24,4 +24,21 @@ class CrontabController < ApplicationController
     render :nothing => true
   end
   
+  def send_tournament_coming_up_emails
+    number_of_days = params[:days_away]
+    
+    start_date = Date.today + number_of_days
+    end_date = start_date + 1.day
+
+    tournaments = Tournament.where("tournament_at >= ? AND tournament_at < ?", start_date, end_date)
+    
+    tournaments.each do |t|
+      t.players.each do |u|
+        TournamentMailer.tournament_coming_up(t, u).deliver_later
+      end
+    end
+    
+    render :nothing => true
+  end
+  
 end
--- app/mailers/tournament_mailer.rb
+++ app/mailers/tournament_mailer.rb
@@ -16,4 +16,12 @@ class TournamentMailer < ApplicationMailer
     mail(to: @user.email, subject: 'EZGolfLeague - Tournament Registration is About to Close')
   end
   
+  def tournament_coming_up(tournament, user)
+    @tournament = tournament
+    @user = user
+    @registration_url = "http://ezgolfleague.herokuapp.com/leagues/#{@tournament.league.id}/tournaments" #TODO: UPDATE
+
+    mail(to: @user.email, subject: 'EZGolfLeague - Your Tournament is Coming Up')
+  end
+  
 end
