af11f4befd4f50dfd30f9839921f99987d6e7948
Hunter Hillegas <hunter@lastonepicked.com>
2015-05-09 08:52:20 -0700

    fix for nil models

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,2 +1,15 @@
 module ContestsHelper
+  
+  def humanize_winners(winners)
+    return nil if winners.blank?
+    
+    html = ""
+    
+    winners.each do |winner|
+      html << "#{winner[:user].complete_name}<br/>"
+    end
+    
+    return html
+  end
+  
 end
