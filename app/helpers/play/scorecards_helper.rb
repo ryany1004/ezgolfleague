b6db3d4859e3f7dac67ca7b3508a47a9f8b3e6a9
Hunter Hillegas <hunter@lastonepicked.com>
2015-06-20 13:36:42 -0700

    tweak scorecard caching

--- app/helpers/play/scorecards_helper.rb
+++ app/helpers/play/scorecards_helper.rb
@@ -1,10 +1,10 @@
 module Play::ScorecardsHelper
   
-  def cache_key_for_scorecard_forms
+  def cache_key_for_scorecard(scorecard_id)
     count = Scorecard.count
     max_updated_at = Scorecard.maximum(:updated_at).try(:utc).try(:to_s, :number)
     
-    return "scorecards/all-#{count}-#{max_updated_at}"
+    return "scorecards/#{scorecard_id}-#{count}-#{max_updated_at}"
   end
   
 end
