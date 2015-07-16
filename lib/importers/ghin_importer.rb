44c8f50d4d2198d62e84379a8cf5bf4588eacd0d
Hunter Hillegas <hunter@lastonepicked.com>
2015-07-16 12:09:02 -0700

    basic importer for GHIN

--- lib/importers/ghin_importer.rb
+++ lib/importers/ghin_importer.rb
@@ -7,14 +7,27 @@ module Importers
       return nil if user.blank? || user.ghin_number.blank?
       
       url = "http://widgets.ghin.com/HandicapLookupResults.aspx?entry=1&ghinno=#{user.ghin_number}&css=default&dynamic=&small=0&mode=&tab=0"
+      
+      puts "Reading: #{url}"
       doc = Nokogiri::HTML(open(url))
-      handicap_index = doc.search("//td[@class='ClubGridHandicapIndex']").children.last.children.to_s.to_f
       
-      logger.info { "HI: #{handicap_index}" }
+      unless doc.blank?
+        root_node = doc.search("//td[@class='ClubGridHandicapIndex']")
+        
+        unless root_node.blank?
+          handicap_index = root_node.children.last.children.to_s.to_f
       
-      unless handicap_index.blank?
-        user.handicap_index = handicap_index
-        user.save
+          puts "HI: #{handicap_index}"
+      
+          unless handicap_index.blank?
+            user.handicap_index = handicap_index
+            user.save
+          end
+        else
+          puts "Root node was blank"
+        end
+      else
+        puts "Doc Was Blank"
       end
     end
     
