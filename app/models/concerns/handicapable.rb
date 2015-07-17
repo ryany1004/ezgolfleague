d4daf7325b30d968a68a64459d2401be7de6d347
Hunter Hillegas <hunter@lastonepicked.com>
2015-07-16 20:24:28 -0700

    update handicap

--- app/models/concerns/handicapable.rb
+++ app/models/concerns/handicapable.rb
@@ -5,7 +5,7 @@ module Handicapable
   def course_handicap(selected_course, course_tee_box)
     return nil if self.handicap_index.blank? or course_tee_box.blank?
     
-    return (self.handicap_index * course_tee_box.slope / 113.0).round
+    return (self.handicap_index.to_f * (course_tee_box.slope.to_f / 113.0)).round
   end
   
 end
