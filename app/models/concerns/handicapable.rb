b3ca9ac9b28002e9999162639a7d94f49b1c10d0
Hunter Hillegas <hunter@lastonepicked.com>
2015-07-16 20:36:13 -0700

    add logging to handicap

--- app/models/concerns/handicapable.rb
+++ app/models/concerns/handicapable.rb
@@ -5,7 +5,12 @@ module Handicapable
   def course_handicap(selected_course, course_tee_box)
     return nil if self.handicap_index.blank? or course_tee_box.blank?
     
-    return (self.handicap_index.to_f * (course_tee_box.slope.to_f / 113.0)).round
+    handicap = (self.handicap_index.to_f * (course_tee_box.slope.to_f / 113.0)).round
+    
+    Rails.logger.debug { "U: #{self.id} HI: #{self.handicap_index.to_f} Slope: #{course_tee_box.slope.to_f}" }
+    Rails.logger.debug { "Handicap: #{handicap}" }
+
+    return handicap
   end
   
 end
