b335eca850a2c962932beedfb171e63f8f756b1e
Hunter Hillegas <hunter@lastonepicked.com>
2015-07-19 14:44:22 -0700

    change reserved word

--- db/migrate/20150719181639_add_details_to_payment.rb
+++ db/migrate/20150719181639_add_details_to_payment.rb
@@ -8,8 +8,8 @@ class AddDetailsToPayment < ActiveRecord::Migration
     
     Tournament.all.each do |t|
       t.players.each do |p|
-        Payment.create(tournament: t, payment_amount: t.dues_amount * -1.0, user: p, payment_method: "Tournament Dues")
-        Payment.create(tournament: t, payment_amount: t.dues_amount, user: p, payment_method: "System Credit")
+        Payment.create(tournament: t, payment_amount: t.dues_amount * -1.0, user: p, payment_source: "Tournament Dues")
+        Payment.create(tournament: t, payment_amount: t.dues_amount, user: p, payment_source: "System Credit")
       end
     end
     
