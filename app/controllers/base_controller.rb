3ea7d0a7fd20cb3c6c647d48854a856360b267ed
Hunter Hillegas <hunter@lastonepicked.com>
2015-12-02 16:15:08 -0800

    force ssl

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,11 @@
 class BaseController < ActionController::Base
   layout "application"
   
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
+  
   before_action :authenticate_user!
 end
