f0d6b824dbc5c524bf0cfce69d708fccc5c5dd56
Hunter Hillegas <hunter@lastonepicked.com>
2016-01-26 17:51:32 -0800

    update text and change is_past requirement

--- app/models/fetching_tools/scorecard_fetching.rb
+++ app/models/fetching_tools/scorecard_fetching.rb
@@ -6,7 +6,7 @@ module FetchingTools
       tournament_day = scorecard.golf_outing.tournament_group.tournament_day
       tournament = tournament_day.tournament
     
-      if tournament.is_past? && tournament_day.game_type.allow_teams == GameTypes::TEAMS_DISALLOWED #in the past, non-team tournament
+      if tournament_day.game_type.allow_teams == GameTypes::TEAMS_DISALLOWED #in the past, non-team tournament
         other_scorecards = []
       else
         other_scorecards = tournament_day.related_scorecards_for_user(scorecard.golf_outing.user)
