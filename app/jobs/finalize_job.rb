e8693bf3c6fc25ad53453fed917a08f081bd881b
Hunter Hillegas <hunter@lastonepicked.com>
2016-02-01 14:10:02 -0800

    job update

--- app/jobs/finalize_job.rb
+++ app/jobs/finalize_job.rb
@@ -1,6 +1,6 @@
 class FinalizeJob < ProgressJob::Base
   def initialize(tournament)
-    super progress_max: tournament.tournament_days.count + 1
+    super progress_max: tournament.tournament_days.count * 3
     
     @tournament = tournament
   end
@@ -8,8 +8,12 @@ class FinalizeJob < ProgressJob::Base
   def perform
     update_stage('Finalizing Tournament')
 
-    @tournament.tournament_days.each do |day|
+    @tournament_days = @tournament.tournament_days.includes(tournament_groups: [golf_outings: [:user, scorecard: :scores]])
+
+    @tournament_days.each do |day|
       day.score_users
+      
+      update_progress
       
       day.assign_payouts_from_scores
       
