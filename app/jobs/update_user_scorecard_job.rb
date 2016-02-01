dbe8798d2c57757676b82c50a40d9d53da2674ed
Hunter Hillegas <hunter@lastonepicked.com>
2016-01-31 17:05:06 -0800

    update scores backgrond

---
+++ app/jobs/update_user_scorecard_job.rb
@@ -0,0 +1,39 @@
+class UpdateUserScorecardJob < ProgressJob::Base
+  def initialize(primary_scorecard, other_scorecards)
+    super progress_max: 2 + other_scorecards.count
+    
+    @primary_scorecard = primary_scorecard
+    @other_scorecards = other_scorecards
+  end
+
+  def perform
+    update_stage('Re-Scoring User')
+
+    Rails.logger.info { "Re-Scoring User" }
+    @primary_scorecard.tournament_day.score_user(@primary_scorecard.golf_outing.user)
+    
+    update_progress
+    
+    Rails.logger.info { "User Re-Scored, Updating After-Action" }
+    @primary_scorecard.tournament_day.game_type.after_updating_scores_for_scorecard(@primary_scorecard)
+  
+    Rails.logger.info { "Updating Other Scorecards" }
+    @other_scorecards.each do |other_scorecard|
+      @primary_scorecard.tournament_day.score_user(other_scorecard.golf_outing.user) unless other_scorecard.golf_outing.blank?
+      @primary_scorecard.tournament_day.game_type.after_updating_scores_for_scorecard(other_scorecard)
+      
+      update_progress
+    end
+    
+    Rails.cache.delete(@primary_scorecard.tournament_day.leaderboard_api_cache_key)
+    
+    unless @primary_scorecard.tournament_day.tournament_day_results.where(:user_primary_scorecard_id => @primary_scorecard.id).blank?
+      Rails.logger.info { "SCORE: Re-Scoring For Scorecard: #{@primary_scorecard.id}. User: #{@primary_scorecard.golf_outing.user.complete_name}. Net Score: #{@primary_scorecard.tournament_day.tournament_day_results.where(:user_primary_scorecard_id => @primary_scorecard.id).first.net_score}" }
+    end
+    
+    update_progress
+    
+    Rails.logger.info { "UpdateUserScorecardJob Completed" }
+  end
+  
+end
---
+++ app/models/updaters/scorecard_updating.rb
@@ -0,0 +1,39 @@
+module UpdatingTools
+  class ScorecardUpdating
+    
+    def self.update_scorecards_for_scores(scores, primary_scorecard, other_scorecards)
+      scores.each do |score_param|
+        Rails.logger.debug { "#{score_param}" }
+      
+        score_id = score_param[0].to_i
+        strokes = score_param[1][:strokes]
+        date_scored = score_param[1][:date_scored]
+      
+        Rails.logger.debug { "#{score_id} #{strokes}" }
+        
+        unless strokes.blank? or score_id.blank?
+          score = Score.find(score_id)
+          
+          should_update = true
+          unless date_scored.blank?
+            scored_at = Time.at(date_scored).to_datetime
+          
+            should_update = false if scored_at <= score.updated_at
+          end
+          
+          if should_update == true
+            score.strokes = strokes
+            score.save
+            
+            Rails.logger.info { "Updating Score: #{score}" }
+          else
+            Rails.logger.info { "Not Updating Scores - Too Old #{date_scored}" }
+          end
+        end
+      end
+    
+      Delayed::Job.enqueue UpdateUserScorecardJob.new(primary_scorecard, other_scorecards)
+    end
+    
+  end
+end
