cebc520ce2be039180a62ad243d6584af8aabcec
Hunter Hillegas <hunter@lastonepicked.com>
2016-02-01 17:07:02 -0800

    update ghin importer

--- lib/importers/ghin_importer.rb
+++ lib/importers/ghin_importer.rb
@@ -4,7 +4,7 @@ module Importers
   class GHINImporter
     
     def self.import_for_all_users
-      User.where("ghin_number IS NOT NULL").each do |u|
+      User.where("ghin_number IS NOT NULL").order("updated_at").each do |u|
         Importers::GHINImporter.import_ghin_for_user(user)
       end
     end
@@ -12,28 +12,32 @@ module Importers
     def self.import_ghin_for_user(user)
       return nil if user.blank? || user.ghin_number.blank?
       
-      url = "http://widgets.ghin.com/HandicapLookupResults.aspx?entry=1&ghinno=#{user.ghin_number}&css=default&dynamic=&small=0&mode=&tab=0"
+      begin
+        url = "http://widgets.ghin.com/HandicapLookupResults.aspx?entry=1&ghinno=#{user.ghin_number}&css=default&dynamic=&small=0&mode=&tab=0"
       
-      puts "Reading: #{url}"
-      doc = Nokogiri::HTML(open(url))
+        puts "Reading: #{url}"
+        doc = Nokogiri::HTML(open(url))
       
-      unless doc.blank?
-        root_node = doc.search("//td[@class='ClubGridHandicapIndex']")
+        unless doc.blank?
+          root_node = doc.search("//td[@class='ClubGridHandicapIndex']")
         
-        unless root_node.blank?
-          handicap_index = root_node.children.last.children.to_s.to_f
+          unless root_node.blank?
+            handicap_index = root_node.children.last.children.to_s.to_f
 
-          unless handicap_index.blank?
-            puts "Handicap Index: #{handicap_index}"
+            unless handicap_index.blank?
+              puts "Handicap Index: #{handicap_index}"
             
-            user.handicap_index = handicap_index
-            user.save
+              user.handicap_index = handicap_index
+              user.save
+            end
+          else
+            puts "Root node was blank"
           end
         else
-          puts "Root node was blank"
-        end
-      else
-        puts "Doc was blank"
+          puts "Doc was blank"
+        end        
+      rescue => e
+        puts "Exception: #{e}"
       end
     end
     
