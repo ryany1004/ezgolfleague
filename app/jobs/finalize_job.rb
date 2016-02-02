25a84b7b4f18e188f2974a1a4f988858e8037977
Hunter Hillegas <hunter@lastonepicked.com>
2016-02-01 16:38:38 -0800

    remove scoring from finalize job

--- app/jobs/finalize_job.rb
+++ app/jobs/finalize_job.rb
@@ -1,6 +1,6 @@
 class FinalizeJob < ProgressJob::Base
   def initialize(tournament)
-    super progress_max: (tournament.tournament_days.count * 3) + (tournament.players.count * tournament.tournament_days.count)
+    super progress_max: (tournament.tournament_days.count * 2)
     
     @tournament = tournament
   end
@@ -14,15 +14,6 @@ class FinalizeJob < ProgressJob::Base
     Rails.logger.info { "Finalize: Starting Job" }
 
     tournament_days.each do |day|      
-      Rails.logger.info { "Finalize: Updating Scores" }
-      players.each do |player|
-        day.score_user(player)
-        
-        update_progress
-      end
-      
-      update_progress
-      
       Rails.logger.info { "Finalize: Assigning Payouts" }
       day.assign_payouts_from_scores
       
