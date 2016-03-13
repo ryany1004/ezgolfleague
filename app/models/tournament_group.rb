64e6f3c2948f2dcd0a61a0f6fe92f77a56e59dd8
Hunter Hillegas <hunter@lastonepicked.com>
2016-03-13 16:14:01 -0700

    do not allow populated tee groups to be deleted

--- app/models/tournament_group.rb
+++ app/models/tournament_group.rb
@@ -1,21 +1,21 @@
 class TournamentGroup < ActiveRecord::Base
   include Servable
-  
+
   belongs_to :tournament_day, inverse_of: :tournament_groups, :touch => true
   has_many :golf_outings, inverse_of: :tournament_group, :dependent => :destroy
-  
+
   paginates_per 50
-  
+
   def players_signed_up
     players = []
-    
+
     self.golf_outings.includes(:user).each do |g|
       players << g.user
     end
-    
+
     return players
   end
-  
+
   def formatted_tee_time
     if self.tournament_day.tournament.show_players_tee_times == true
       return self.tee_time_at.to_s(:time_only)
@@ -23,16 +23,16 @@ class TournamentGroup < ActiveRecord::Base
       return "#{self.tee_time_at.to_s(:time_only)} - #{self.time_description}"
     end
   end
-  
+
   def time_description
     count = self.tournament_day.tournament_groups.count
     index = self.tournament_day.tournament_groups.index(self)
-    
+
     early = 0
     middle = (count / 3)
     late = (count / 3) * 2
-    
-    if index >= early && index < middle 
+
+    if index >= early && index < middle
       return "Early"
     elsif index >= middle && index < late
       return "Middle"
@@ -40,7 +40,7 @@ class TournamentGroup < ActiveRecord::Base
       return "Late"
     end
   end
-  
+
   def api_time_description
     if self.tournament_day.tournament.show_players_tee_times == true
       return self.tee_time_at.to_s(:time_only)
@@ -48,7 +48,15 @@ class TournamentGroup < ActiveRecord::Base
       return self.time_description
     end
   end
-  
+
+  def can_be_deleted?
+    if self.golf_outings.count > 0
+      return false
+    else
+      return true
+    end
+  end
+
   #date parsing
   def tee_time_at=(date)
     begin
@@ -60,7 +68,7 @@ class TournamentGroup < ActiveRecord::Base
   end
 
   #JSON
-  
+
   def as_json(options={})
     super(
       :only => [:tee_time_at, :max_number_of_players],
