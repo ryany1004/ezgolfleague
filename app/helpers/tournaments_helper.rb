02bfba3f56a5df723fd28e46bed19750ea136f90
Hunter Hillegas <hunter@lastonepicked.com>
2016-03-19 07:49:42 -0700

    temporary editable override

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,18 +1,20 @@
 module TournamentsHelper
-  
+
   def is_editable?(tournament)
-    return false if tournament.blank?
-    
-    if is_today?(tournament)
-      return false
-    else
-      return true
-    end
+    # return false if tournament.blank?
+    #
+    # if is_today?(tournament)
+    #   return false
+    # else
+    #   return true
+    # end
+
+    return true
   end
-  
+
   def is_today?(tournament)
     return false if tournament.tournament_days.count == 0
-    
+
     if tournament.first_day.tournament_at >= Time.zone.now.at_beginning_of_day && tournament.last_day.tournament_at < Time.zone.now.at_end_of_day
       return true
     else
@@ -23,9 +25,9 @@ module TournamentsHelper
   def split_scores_for_scorecard(scorecard, number_of_holes)
     return scorecard.scores.each_slice(number_of_holes / 2).to_a
   end
-  
+
   def split_holes_for_course_tournament(tournament)
     return tournament.course_holes.each_slice(tournament.course_holes.count / 2).to_a
   end
-  
+
 end
