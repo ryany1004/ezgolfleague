2f4d4ad644383e4e6206dd992d1a5f70e36deafa
Hunter Hillegas <hunter@lastonepicked.com>
2016-04-15 17:42:35 -0700

    handle course handicap re-calc w/ GHIN

--- lib/importers/ghin_importer.rb
+++ lib/importers/ghin_importer.rb
@@ -2,31 +2,44 @@ require 'open-uri'
 
 module Importers
   class GHINImporter
-    
+
     def self.import_for_all_users
       User.where("ghin_number IS NOT NULL").order("updated_at").each do |u|
         Importers::GHINImporter.import_ghin_for_user(u)
+
+        upcoming_tournaments = Tournament.all_upcoming(u.leagues, nil)
+        upcoming_tournaments.each do |t|
+          t.tournament_days.each do |td|
+            scorecard = td.primary_scorecard_for_user(u)
+
+            unless scorecard.blank?
+              scorecard.set_course_handicap(true) #re-calc the course handicap
+
+              td.touch
+            end
+          end
+        end
       end
     end
-    
+
     def self.import_ghin_for_user(user)
       return nil if user.blank? || user.ghin_number.blank?
-      
+
       begin
         url = "http://widgets.ghin.com/HandicapLookupResults.aspx?entry=1&ghinno=#{user.ghin_number}&css=default&dynamic=&small=0&mode=&tab=0"
-      
+
         puts "Reading: #{url}"
         doc = Nokogiri::HTML(open(url))
-      
+
         unless doc.blank?
           root_node = doc.search("//td[@class='ClubGridHandicapIndex']")
-        
+
           unless root_node.blank?
             handicap_index = root_node.children.last.children.to_s.to_f
 
             unless handicap_index.blank?
               puts "Handicap Index: #{handicap_index}"
-            
+
               unless handicap_index == 0.0
                 user.handicap_index = handicap_index
                 user.save
@@ -39,11 +52,11 @@ module Importers
           end
         else
           puts "Doc was blank"
-        end        
+        end
       rescue => e
         puts "Exception: #{e}"
       end
     end
-    
+
   end
 end
