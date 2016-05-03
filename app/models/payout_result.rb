9a7cf3c9dd70541889650a0b1cdd3bb40805513d
Hunter Hillegas <hunter@lastonepicked.com>
2016-05-02 18:55:43 -0700

    change relationship status

--- app/models/payout_result.rb
+++ app/models/payout_result.rb
@@ -1,5 +1,5 @@
 class PayoutResult < ActiveRecord::Base
-  belongs_to :payout, inverse_of: :payout_result
+  belongs_to :payout, inverse_of: :payout_results
   belongs_to :flight, inverse_of: :payout_results
   belongs_to :user, inverse_of: :payout_results
   belongs_to :tournament_day, inverse_of: :payout_results
