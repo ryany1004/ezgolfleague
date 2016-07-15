17e60fb541de46a663217c8b75413346b7051d1d
Hunter Hillegas <hunter@lastonepicked.com>
2016-07-15 16:36:23 -0700

    enforce thumb size

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
 
   paginates_per 50
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   #this is to work around a Devise bug
