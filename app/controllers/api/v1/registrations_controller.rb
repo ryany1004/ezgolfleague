931998f0aae1f316d2c27382c31f036219e1722e
Hunter Hillegas <hunter@lastonepicked.com>
2016-07-24 13:31:42 -0700

    spelling error on email

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::RegistrationsController < Api::V1::ApiBaseController
   def create
     details = ActiveSupport::JSON.decode(request.body.read)
 
-    email_address = details["emailAddress"]
+    email = details["emailAddress"]
     first_name = details["firstName"]
     last_name = details["lastName"]
     password = details["password"]
     phone_number = details["phoneNumber"]
     ghin_number = details["ghinNumber"]
 
-    user = User.create(email_address: email_address, first_name: first_name, last_name: last_name, password: password, password_confirmation: password, phone_number: phone_number, ghin_number: ghin_number)
+    user = User.create(email: email, first_name: first_name, last_name: last_name, password: password, password_confirmation: password, phone_number: phone_number, ghin_number: ghin_number)
 
     if user.save
       Delayed::Job.enqueue GhinUpdateJob.new([user]) unless user.ghin_number.blank?
