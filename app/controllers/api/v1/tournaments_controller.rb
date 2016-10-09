592c8f337a1283605cf8fd80f956648641d1136e
Hunter Hillegas <hunter@lastonepicked.com>
2016-10-09 14:32:21 -0700

    fix ordering of past

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::TournamentsController < Api::V1::ApiBaseController
 
       todays_tournaments = Tournament.all_today(@current_user.leagues)
       upcoming_tournaments = Tournament.all_upcoming(@current_user.leagues, nil)
-      past_tournaments = Tournament.all_past(@current_user.leagues, nil).limit(8).order("tournament_starts_at DESC")
+      past_tournaments = Tournament.all_past(@current_user.leagues, nil).limit(8).reorder("tournament_starts_at DESC")
 
       all_tournaments = todays_tournaments + upcoming_tournaments + past_tournaments
       all_tournaments = all_tournaments.to_a
