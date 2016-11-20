3f8d3a10c58c36aa687e7c82454c3f1858993601
Hunter Hillegas <hunter@lastonepicked.com>
2016-11-20 13:14:56 -0800

    update stableford scoring per Cevin

--- lib/game_types/individual_stableford_scorecard.rb
+++ lib/game_types/individual_stableford_scorecard.rb
@@ -1,11 +1,11 @@
 module GameTypes
   class IndividualStablefordScorecard < GameTypes::DerivedScorecard
-    
+
     def name(shorten_for_print = false)
       return "Stableford"
     end
-    
-    def calculate_scores    
+
+    def calculate_scores
       new_scores = []
 
       handicap_allowance = self.tournament_day.handicap_allowance(user)
@@ -20,12 +20,12 @@ module GameTypes
       self.scores = new_scores
     end
 
-    def score_for_hole(user, handicap_allowance, hole)      
+    def score_for_hole(user, handicap_allowance, hole)
       scorecard = self.golfer_team.tournament_day.primary_scorecard_for_user(user)
-      
+
       strokes = 0
       strokes = scorecard.scores.where(course_hole: hole).first.strokes
-      
+
       handicap_allowance.each do |h|
         if h[:course_hole] == hole
           if h[:strokes] != 0
@@ -33,30 +33,28 @@ module GameTypes
           end
         end
       end
-      
+
       score = 0
       if self.is_double_eagle?(hole, strokes)
+        score = 16
+      elsif self.is_eagle?(hole, strokes)
         score = 8
-      elsif self.is_double_eagle?(hole, strokes)
-        score = 5
       elsif self.is_birdie?(hole, strokes)
-        score = 2
+        score = 4
       elsif self.is_par?(hole, strokes)
-        score = 0
+        score = 2
       elsif self.is_bogey?(hole, strokes)
-        score = -1
+        score = 1
       elsif self.is_double_bogey_or_worse?(hole, strokes)
-        score = -3
+        score = 0
       end
-      
+
       return score
     end
 
-    #NOTE: The point values can be variable.
-
     def is_double_eagle?(hole, strokes)
       par = hole.par
-      
+
       if par == 4 && strokes == 1
         return true
       elsif par == 5 && strokes == 2
@@ -67,48 +65,55 @@ module GameTypes
         return false
       end
     end
-    
+
+    def is_eagle?(hole, strokes)
+      par = hole.par
+
+      if strokes == par - 2
+        return true
+      else
+        return false
+      end
+    end
+
     def is_birdie?(hole, strokes)
       par = hole.par
-      
+
       if strokes == par - 1
         return true
       else
         return false
       end
     end
-    
+
     def is_par?(hole, strokes)
       par = hole.par
-      
+
       if par == strokes
         return true
       else
         return false
       end
     end
-    
+
     def is_bogey?(hole, strokes)
       par = hole.par
-      
+
       if strokes == par + 1
         return true
       else
         return false
       end
     end
-    
+
     def is_double_bogey_or_worse?(hole, strokes)
       par = hole.par
-      
+
       if strokes > par + 1
         return true
       else
         return false
       end
     end
-    
-    #TODO: albatross is 2 on a par 5
-    
   end
 end
