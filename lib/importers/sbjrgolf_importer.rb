c66d9b091426d88683a80debe8254bb312c96615
Hunter Hillegas <hunter@lastonepicked.com>
2017-07-03 18:15:15 -0700

    mis-spelled

--- lib/importers/sbjrgolf_importer.rb
+++ lib/importers/sbjrgolf_importer.rb
@@ -7,6 +7,8 @@ module Importers
           parent_user = User.create(first_name: user[:parent_first], last_name: user[:parent_last], email: user[:parent_email], password: SecureRandom.uuid, phone_number: user[:phone])
         end
 
+        puts "Parent: #{parent_user.complete_name}"
+
         child_users = User.where(first_name: user[:golfer_first], last_name: user[:golfer_last])
         if child_users.count > 1
           puts "Child Users > 1 #{user[:golfer_first]} #{user[:golfer_last]}"
@@ -16,11 +18,17 @@ module Importers
           if child_user.blank?
             child_user = User.create(first_name: user[:golfer_first], last_name: user[:golfer_last], email: "#{SecureRandom.uuid}@nobody.com", password: SecureRandom.uuid, phone_number: user[:phone])
           end
+          
           child_user.parent_user = parent_user
+          child_user.save
+
+          puts "Set Parent #{parent_user.complete_name} for Child #{child_user.complete_name}"
 
           league = League.where(name: user[:league]).first
           unless league.blank?
             if !child_user.leagues.include? league
+              puts "Assigned to League: #{league.id}"
+
               LeagueMembership.create(league: league, user: child_user)
             end
           end
