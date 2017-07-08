2ae364845540cb89407639d5107cf0912aea377d
Hunter Hillegas <hunter@lastonepicked.com>
2017-07-08 15:27:01 -0700

    Fix for attr_encrypted

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < BaseController
-  before_filter :fetch_tournament_day, except: [:index, :finalization_report]
+  before_action :fetch_tournament_day, except: [:index, :finalization_report]
 
   def index
     leagues = nil
