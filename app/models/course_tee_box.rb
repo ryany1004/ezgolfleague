c64abd2e3a164f2804860803a24e32accf13263f
Hunter Hillegas <hunter@lastonepicked.com>
2017-07-09 14:48:19 -0700

    fix for JSON in course tee box

--- app/models/course_tee_box.rb
+++ app/models/course_tee_box.rb
@@ -1,4 +1,6 @@
 class CourseTeeBox < ApplicationRecord
+  include Servable
+
   has_many :course_hole_tee_boxes, inverse_of: :course_tee_box
   belongs_to :course, inverse_of: :course_tee_boxes, touch: true
 
