f6f852b1ad20ee700b556a2e00a53327d9197443
Hunter Hillegas <hunter@lastonepicked.com>
2017-07-13 16:08:26 -0700

    fix scoring bug

--- app/models/updaters/scorecard_updating.rb
+++ app/models/updaters/scorecard_updating.rb
@@ -1,32 +1,32 @@
 module Updaters
   class ScorecardUpdating
-    
+
     def self.update_scorecards_for_scores(scores, primary_scorecard, other_scorecards)
-      scores.each do |score_param|
+      scores.to_unsafe_h.each do |score_param|
         Rails.logger.info { "score_param #{score_param}" }
-      
+
         score_id = score_param[0].to_i
-        strokes = score_param[1][:strokes]
+        strokes = score_param[1][:strokes].to_i
         date_scored = score_param[1][:date_scored]
-      
+
         Rails.logger.info { "#{score_id} #{strokes}" }
-        
+
         unless strokes.blank? or score_id.blank?
           score = Score.find(score_id)
-          
+
           should_update = true
           unless date_scored.blank?
             scored_at = Time.at(date_scored).to_datetime
-          
+
             should_update = false if scored_at <= score.updated_at
           end
-          
+
           if should_update == true
             score.strokes = strokes
             score.save
-            
+
             Rails.logger.info { "Updating Score: #{score.id}" }
-            
+
             scorecard_to_rescore = score.scorecard
             Delayed::Job.enqueue UpdateUserScorecardJob.new(scorecard_to_rescore, []) unless scorecard_to_rescore.blank?
           else
@@ -34,9 +34,9 @@ module Updaters
           end
         end
       end
-    
+
       Delayed::Job.enqueue UpdateUserScorecardJob.new(primary_scorecard, other_scorecards)
     end
-    
+
   end
 end
