71c5ebef05d5ef72991b342591292828c1e9f067
Hunter Hillegas <hunter@lastonepicked.com>
2017-08-03 14:00:11 -0700

    Mid-way update to AJAX form

--- app/controllers/golf_outings_controller.rb
+++ app/controllers/golf_outings_controller.rb
@@ -9,9 +9,11 @@ class GolfOutingsController < BaseController
   end
 
   def update_players
-    @job = Delayed::Job.enqueue PlayerSignupJob.new(@tournament_day, params)
+    # @job = Delayed::Job.enqueue PlayerSignupJob.new(@tournament_day, params)
 
-    redirect_to league_tournaments_path(current_user.selected_league), :flash => { :warning => "Your player signup submissions are being processed. This process usually takes a few minutes to complete."}
+    # redirect_to league_tournaments_path(current_user.selected_league), :flash => { :warning => "Your player signup submissions are being processed. This process usually takes a few minutes to complete."}
+
+    # render nothing: true
   end
 
   def delete_signup
