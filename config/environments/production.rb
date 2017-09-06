31b4152b8054822c505b11e441c0ea0ca7d09ced
Hunter Hillegas <hunter@lastonepicked.com>
2017-09-06 14:44:11 -0700

    updated sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: ENV['SENDGRID_DOMAIN']
