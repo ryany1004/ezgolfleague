f4b39ad9b13527707c4b713644c917cc248f2389
Hunter Hillegas <hunter@lastonepicked.com>
2017-09-10 15:58:42 -0700

    re-worked scorecard

--- lib/game_types/scorecard_api_base.rb
+++ lib/game_types/scorecard_api_base.rb
@@ -52,7 +52,7 @@ module GameTypes
       #Old Format
       score_info = []
       card.scores.each do |score|
-        score_info << score.strokes.to_s
+        score_info << [score.strokes.to_s]
       end
       score_info << ["#{card.front_nine_score(false)}/#{card.front_nine_score(true)}", "#{card.back_nine_score(false)}/#{card.back_nine_score(true)}"]
       score_info << card.course_handicap.to_s
