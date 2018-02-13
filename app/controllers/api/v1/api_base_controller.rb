bf0023e9430fe7a024cb13a867f839069752c349
Hunter Hillegas <hunter@lastonepicked.com>
2018-02-12 17:16:55 -0800

    timezone support for API

--- app/controllers/api/v1/api_base_controller.rb
+++ app/controllers/api/v1/api_base_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ApiBaseController < ApplicationController
   skip_before_action :verify_authenticity_token
+  around_action :user_time_zone, if: :current_user
+
   force_ssl if: :ssl_configured?
 
   def protect_with_token
@@ -21,6 +23,8 @@ class Api::V1::ApiBaseController < ApplicationController
 
         render text: "No such session", :status => :unauthorized
       else
+        sign_in(@current_user, :bypass => true)
+
         Rails.logger.info { "API: Login Successful for #{@current_user.id}" }
       end
     end
@@ -31,8 +35,11 @@ class Api::V1::ApiBaseController < ApplicationController
     user.save
   end
 
+  def user_time_zone(&block)
+    Time.use_zone(current_user.time_zone, &block)
+  end
+
   def ssl_configured?
     !Rails.env.development?
   end
-
 end
