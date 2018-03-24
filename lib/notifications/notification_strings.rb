8d3345df8353104c00cdbdb0020918ae8053e8c5
Hunter Hillegas <hunter@lastonepicked.com>
2018-03-24 14:10:45 -0700

    finalization string update

--- lib/notifications/notification_strings.rb
+++ lib/notifications/notification_strings.rb
@@ -1,11 +1,11 @@
 module Notifications
 	class NotificationStrings
 	
-		def first_time_finalize(tournament_name = "your tournament")
+		def self.first_time_finalize(tournament_name = "your tournament")
 			return "Final results are now available for #{tournament_name}. Tap for more info."
 		end
 
-		def update_finalize(tournament_name = "your tournament")
+		def self.update_finalize(tournament_name = "your tournament")
 			return "Final results have been updated for #{tournament_name}. Tap for more info."
 		end
 
