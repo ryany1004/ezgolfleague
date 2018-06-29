38bfb9f8477ab923b3e1bd9f36980d3e60c250ed
Hunter Hillegas <hunter@lastonepicked.com>
2018-06-29 11:18:45 -0700

    wrap notification in rescue

--- app/jobs/send_notifications_job.rb
+++ app/jobs/send_notifications_job.rb
@@ -1,27 +1,31 @@
 class SendNotificationsJob < ApplicationJob
   def perform
-    @templates = NotificationTemplate.where("deliver_at <= ?", DateTime.now).where("has_been_delivered = ?", false)
+    begin
+      @templates = NotificationTemplate.where("deliver_at <= ?", DateTime.now).where("has_been_delivered = ?", false)
 
-    @templates.each do |t|
-      t.recipients.each do |r|
-        Notification.create(notification_template: t, user: r, title: t.title, body: t.body)
+      @templates.each do |t|
+        t.recipients.each do |r|
+          Notification.create(notification_template: t, user: r, title: t.title, body: t.body)
 
-        email_from = 'support@ezgolfleague.com'
+          email_from = 'support@ezgolfleague.com'
 
-        if t.league.blank?
-          subject = t.title
-        else
-          subject = "#{t.league.name} - #{t.title}"
-          email_from = t.league.league_admins.first.email
+          if t.league.blank?
+            subject = t.title
+          else
+            subject = "#{t.league.name} - #{t.title}"
+            email_from = t.league.league_admins.first.email
+          end
+
+          NotificationMailer.notification_message(r, email_from, subject, t.body).deliver_later if r.wants_email_notifications == true
+
+          r.send_mobile_notification("#{t.title}: #{t.body}") if r.wants_push_notifications == true
         end
 
-        NotificationMailer.notification_message(r, email_from, subject, t.body).deliver_later if r.wants_email_notifications == true
-
-        r.send_mobile_notification(t.title) if r.wants_push_notifications == true
+        t.has_been_delivered = true
+        t.save
       end
-
-      t.has_been_delivered = true
-      t.save
+    rescue => e
+      Rails.logger.info { "Error Sending Notifications: #{e}" }
     end
   end
 end
