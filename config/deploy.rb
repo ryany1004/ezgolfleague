01ca0bbacfcbb1c329b5e22a19c35d82345f3c58
Hunter Hillegas <hunter@lastonepicked.com>
2018-07-03 08:13:01 -0700

    memcache clear

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,7 +71,7 @@ namespace :deploy do
   after :publishing, :restart
   after :publishing, :fix_permissions
   after :finished, 'resque:restart'
-  after :finished, execute :sudo, ("cd #{deploy_to}/current && /usr/bin/env rake memcached_clear RAILS_ENV=production")
+  after :finished, execute :sudo, "cd #{deploy_to}/current && /usr/bin/env rake memcached_clear RAILS_ENV=production"
 
   set :rollbar_token, '75d79ff8ca4643809de5616d7c6c2265'
   set :rollbar_env, Proc.new { fetch :stage }
