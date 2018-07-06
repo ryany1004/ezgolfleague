28284e241a4d6c9039dbacf0422da807c728055f
Hunter Hillegas <hunter@lastonepicked.com>
2018-07-06 11:51:46 -0700

    reduce past tournaments to 8

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::TournamentsController < Api::V1::ApiBaseController
 
         todays_tournaments = Tournament.all_today(@current_user.leagues)
         upcoming_tournaments = Tournament.all_upcoming(@current_user.leagues, nil)
-        past_tournaments = Tournament.all_past(@current_user.leagues, nil).limit(15).reorder("tournament_starts_at DESC")
+        past_tournaments = Tournament.all_past(@current_user.leagues, nil).limit(8).reorder("tournament_starts_at DESC")
 
         all_tournaments = todays_tournaments + upcoming_tournaments + past_tournaments
         all_tournaments = all_tournaments.select {|t| t.all_days_are_playable? }.to_a #only include tournaments with all playable days
