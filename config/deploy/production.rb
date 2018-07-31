29636b2bcfcd922f0bea4afbde6d74327538e04e
Hunter Hillegas <hunter@lastonepicked.com>
2018-07-31 16:52:43 -0700

    staging for staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,4 @@ server 'production.ezgolfleague.com', roles: %w{web app db}, user: "root"
 set :deploy_to, "/var/web/production/"
 set :stage, :production
 set :rails_env, :production
+set :branch, 'master'
