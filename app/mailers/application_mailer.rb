27d77f961c8db277ea91ac5a7cb1a1485caae3e7
Hunter Hillegas <hunter@lastonepicked.com>
2018-08-02 09:53:59 -0700

    do not send mail in staging

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "support@ezgolfleague.com"
   layout 'mailer'
+
+  before_action :check_before_sending
+
+  private
+
+  def check_before_sending
+    if Rails.env.staging?
+      mail.perform_deliveries = false
+    end
+  end
 end
