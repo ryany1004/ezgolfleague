a452f7786c7f8cbe91a4f0a1e968e11782cc47bc
Hunter Hillegas <hunter@lastonepicked.com>
2018-08-05 08:48:21 -0700

    Explicit Resque queue

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,42 +7,11 @@ set :repo_url, "git@github.com:dopp10/ezgolfleague.git"
 set :stages, ["staging", "production"]
 set :default_stage, "production"
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
 role :resque_worker, "production.ezgolfleague.com"
 role :resque_scheduler, "production.ezgolfleague.com"
 
 set :resque_environment_task, true
 set :resque_log_file, "log/resque.log"
-set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
 
 namespace :deploy do
   desc 'Restart application'
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   config.active_job.queue_adapter     = :resque
   config.active_job.queue_name_prefix = "staging_ezgolfleague_#{Rails.env}"
 
+<<<<<<< Updated upstream
   ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
@@ -98,6 +99,10 @@ Rails.application.configure do
     password: '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',
     domain: 'ezgolfleague.com'
   }
+=======
+
+  config.action_mailer.perform_deliveries = false
+>>>>>>> Stashed changes
 
   config.paperclip_defaults = {
     storage: :s3,
