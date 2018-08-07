8afc238121413c7dce67828da4113896cd0dd962
Hunter Hillegas <hunter@lastonepicked.com>
2018-08-06 21:51:51 -0700

    fix for deploy files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,6 @@ server 'production.ezgolfleague.com', roles: %w{web app db}, user: "root"
 set :deploy_to, "/var/web/production/"
 set :stage, :production
 set :rails_env, :production
-<<<<<<< Updated upstream
-=======
 set :branch, 'master'
 set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
 set :resque_rails_env, "production"
->>>>>>> Stashed changes
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,9 +3,6 @@ server 'production.ezgolfleague.com', roles: %w{web app db}, user: "root"
 set :deploy_to, "/var/web/staging/"
 set :stage, :staging
 set :rails_env, :staging
-<<<<<<< Updated upstream
-=======
 set :branch, 'staging'
 set :workers, { "staging" => 2 }
 set :resque_rails_env, "staging"
->>>>>>> Stashed changes
