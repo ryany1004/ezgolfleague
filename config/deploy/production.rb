19d4b5a03b37b0aca9f858ecd4d08eee44e453de
Hunter Hillegas <hunter@lastonepicked.com>
2018-08-10 16:15:48 -0700

    Update queues

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,5 @@ set :deploy_to, "/var/web/production/"
 set :stage, :production
 set :rails_env, :production
 set :branch, 'master'
-set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
+set :workers, { "default" => 4, "ezgolfleague_production_mailers" => 1 }
 set :resque_rails_env, "production"
