2bd49caff27bc7e6b1f047b762b4d1e487098eff
Hunter Hillegas <hunter@lastonepicked.com>
2018-08-10 17:07:10 -0700

    Try the async job adapter in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -85,8 +85,10 @@ Rails.application.configure do
   config.enable_dependency_loading = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = "staging_ezgolfleague_#{Rails.env}"
+  #config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter     = :async
+  config.active_job.queue_name_prefix = Rails.env
+
   config.action_mailer.perform_deliveries = false
 
   config.paperclip_defaults = {
