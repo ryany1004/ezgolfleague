ab251a9e9b16e61d9d3a7b1266fa23e498231f52
Hunter Hillegas <hunter@lastonepicked.com>
2018-08-11 13:27:13 -0700

    Move displayable league season ranking group into method

--- app/models/league_season_ranking_group.rb
+++ app/models/league_season_ranking_group.rb
@@ -4,6 +4,10 @@ class LeagueSeasonRankingGroup < ApplicationRecord
 	belongs_to :league_season, inverse_of: :league_season_ranking_groups, touch: true
 	has_many :league_season_rankings, ->{ order 'rank' }, inverse_of: :league_season_ranking_group, dependent: :destroy
 
+  def displayable_league_season_rankings
+    league_season_rankings.where("points > 0 OR payouts > 0")
+  end
+
   #JSON
 
   def as_json(options={})
