2db8fd61c7d4f329da935476f25ff708f056a26a
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-05 16:09:30 -0700

    Put resque back

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,5 @@
 server 'production.ezgolfleague.com', roles: %w{web app db}, user: "root"
 
-role :resque_worker, "production.ezgolfleague.com"
-role :resque_scheduler, "production.ezgolfleague.com"
-
 set :deploy_to, "/var/web/production/"
 set :stage, :production
 set :rails_env, :production
