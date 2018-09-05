f7b78fe7efa4c57201d027a09482f85895263f92
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-05 16:59:46 -0700

    Add  user to role

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,5 +7,5 @@ set :branch, 'staging'
 set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
 set :resque_rails_env, "staging"
 
-role :resque_worker, "staging.ezgolfleague.com"
-role :resque_scheduler, "staging.ezgolfleague.com"
+role :resque_worker, "staging.ezgolfleague.com", user: "root"
+role :resque_scheduler, "staging.ezgolfleague.com", user: "root"
