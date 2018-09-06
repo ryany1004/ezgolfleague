1ae500570ead78f4ec138ac159d178cfeda43454
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-05 17:14:43 -0700

    Resque stage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 server 'production.ezgolfleague.com', roles: %w{web app db}, user: "root"
 
+role :resque_worker, "production.ezgolfleague.com", user: "root"
+role :resque_scheduler, "production.ezgolfleague.com", user: "root"
+
 set :deploy_to, "/var/web/production/"
 set :stage, :production
 set :rails_env, :production
 set :branch, 'master'
 set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
 set :resque_rails_env, "production"
-
-role :resque_worker, "production.ezgolfleague.com", user: "root"
-role :resque_scheduler, "production.ezgolfleague.com", user: "root"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,11 @@
 server 'staging.ezgolfleague.com', roles: %w{web app db}, user: "root"
 
+role :resque_worker, "staging.ezgolfleague.com", user: "root"
+role :resque_scheduler, "staging.ezgolfleague.com", user: "root"
+
 set :deploy_to, "/var/web/staging/"
 set :stage, :staging
 set :rails_env, :staging
 set :branch, 'staging'
 set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
 set :resque_rails_env, "staging"
-
-role :resque_worker, "staging.ezgolfleague.com", user: "root"
-role :resque_scheduler, "staging.ezgolfleague.com", user: "root"
