1d0fcfe6cd80b47159533640752f86012e53b055
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-05 17:04:38 -0700

    Add role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,5 +7,5 @@ set :branch, 'master'
 set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
 set :resque_rails_env, "production"
 
-role :resque_worker, "production.ezgolfleague.com"
-role :resque_scheduler, "production.ezgolfleague.com"
+role :resque_worker, "production.ezgolfleague.com", user: "root"
+role :resque_scheduler, "production.ezgolfleague.com", user: "root"
