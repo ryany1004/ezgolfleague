c84a19ea9b8f7525c2fde3df1c827664085f2428
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-08 09:31:37 -0700

    Increase workers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,5 @@ set :stage, :production
 set :rails_env, :production
 set :branch, 'master'
 
-set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
+set :workers, { "default" => 4, "ezgolfleague_production_mailers" => 2 }
 set :resque_rails_env, "production"
