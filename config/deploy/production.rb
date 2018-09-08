e24faf2c49a994d0dbe22245148624aacbdfe180
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-08 09:37:13 -0700

    Fix for deploy file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,9 @@
-server 'production.ezgolfleague.com', roles: %w{web app db}, user: "root"
+server 'production.ezgolfleague.com', roles: %w{web app db resque_worker resque_scheduler}, user: "root"
 
 set :deploy_to, "/var/web/production/"
 set :stage, :production
 set :rails_env, :production
 set :branch, 'master'
-set :workers, { "default" => 2, "ezgolfleague_production_mailers" => 1 }
-set :resque_rails_env, "production"
 
-role :resque_worker, "production.ezgolfleague.com"
-role :resque_scheduler, "production.ezgolfleague.com"
+set :workers, { "default" => 4, "ezgolfleague_production_mailers" => 2 }
+set :resque_rails_env, "production"
