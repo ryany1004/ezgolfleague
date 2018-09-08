5967f62a1df7cff48a4bc893e3899ceba5eba216
Hunter Hillegas <hunter@lastonepicked.com>
2018-09-08 09:36:01 -0700

    date update in template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ApplicationMailer
   end
 
   def invite(email_address, league)
+  	email_address = email_address.strip
   	@league = league
 
   	mail(to: email_address, from: 'support@ezgolfleague.com', subject: "You've been invited to join #{league.name} via EZ Golf League")
