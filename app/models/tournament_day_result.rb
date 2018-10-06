cdb28a9f214a6e98df491cae78428bfcbb1e99d3
Hunter Hillegas <hunter@lastonepicked.com>
2018-10-06 10:55:01 -0700

    Touch flights when adding results

--- app/models/tournament_day_result.rb
+++ app/models/tournament_day_result.rb
@@ -4,7 +4,7 @@ class TournamentDayResult < ApplicationRecord
   belongs_to :tournament_day, inverse_of: :tournament_day_results, touch: true
   belongs_to :user, inverse_of: :tournament_day_results
   belongs_to :primary_scorecard, :class_name => "Scorecard", :foreign_key => "user_primary_scorecard_id"
-  belongs_to :flight, inverse_of: :tournament_day_results
+  belongs_to :flight, inverse_of: :tournament_day_results, touch: true
 
   validates :name, presence: true
 
