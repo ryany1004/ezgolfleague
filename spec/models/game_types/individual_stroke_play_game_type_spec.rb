a190c1a2ac6ac2436e916755bec205598e3c6971
Hunter Hillegas <hunter@lastonepicked.com>
2018-11-04 16:17:03 -0800

    Tests for complete play-through

--- spec/models/game_types/individual_stroke_play_game_type_spec.rb
+++ spec/models/game_types/individual_stroke_play_game_type_spec.rb
@@ -17,9 +17,34 @@ describe "Individual Stroke Play" do
 
   it "#user_is_in_group?"
 
-  it "#setup_partial"
+  it "#setup_partial" do
+    expect(generic_stroke_play.setup_partial).to eq("shared/game_type_setup/individual_stroke_play")
+  end
 
-  it "#can_be_played?"
+  it "#can_be_played?" do
+    tournament = create_stroke_play_tournament(strokes: [1,1,6,4,6,3,5,6,5,7,6,5,3,6,6,5,3,10])
 
-  it "#related_scorecards_for_user"
+    expect(tournament.first_day.can_be_played?).to eq(true)
+  end
+
+  it "#related_scorecards_for_user" do
+    tournament = create_two_person_stroke_play_tournament
+    golfer = tournament.players.first
+
+    expect(tournament.first_day.game_type.related_scorecards_for_user(golfer).count).to eq(1)
+  end
+
+  it "verify_results" do
+    tournament = create_stroke_play_tournament(strokes: [1,1,6,4,6,3,5,6,5,7,6,5,3,6,6,5,3,10])
+    result = tournament.first_day.tournament_day_results.first
+
+    expect(result.net_score).to eq(76)
+    expect(result.gross_score).to eq(88)
+    expect(result.back_nine_net_score).to eq(45)
+    expect(result.front_nine_net_score).to eq(31)
+    expect(result.front_nine_gross_score).to eq(37)
+    expect(result.par_related_net_score).to eq(5)
+    expect(result.par_related_gross_score).to eq(17)
+    expect(result.adjusted_score).to eq(85)
+  end
 end
