3a3e8d6bb0462f167f55568c73bd5892ac7d02f3
Hunter Hillegas <hunter@lastonepicked.com>
2018-11-18 08:44:58 -0800

    label_method hashrocket

--- app/models/tournament_day_result.rb
+++ app/models/tournament_day_result.rb
@@ -3,7 +3,7 @@ class TournamentDayResult < ApplicationRecord
 
   belongs_to :tournament_day, inverse_of: :tournament_day_results, touch: true
   belongs_to :user, inverse_of: :tournament_day_results
-  belongs_to :primary_scorecard, :class_name => "Scorecard", :foreign_key => "user_primary_scorecard_id"
+  belongs_to :primary_scorecard, class_name: "Scorecard", foreign_key: "user_primary_scorecard_id"
   #TEAM: does this need to be primary_scorecard(s) instead?
   #TEAM: do we also tie a TDR to the ScoringRule that generated it so we can display results by scoring rule
 
