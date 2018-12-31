288011be3e00bc24d3fe6464bb21e4cb09d222cc
Hunter Hillegas <hunter@lastonepicked.com>
2018-12-30 18:03:14 -0800

    UI updates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < BaseController
 	def index
-		@next_tournament = Tournament.all_upcoming(current_user.leagues_admin).first
+		@next_tournament = Tournament.all_upcoming(current_user.leagues_admin).limit(1).first
+		@previous_tournament = Tournament.all_past(current_user.leagues_admin).reorder(tournament_starts_at: :desc).limit(1).first
 
 		@league_season = current_user.active_league_season
 		@ranking_groups = @league_season.league_season_ranking_groups
