4b8fe4095769f62949ae5efb59c9edff1a9aa86e
Hunter Hillegas <hunter@lastonepicked.com>
2019-02-27 16:03:39 -0800

    Don’t process if we aren’t passed a valid season

--- app/jobs/rank_league_season_job.rb
+++ app/jobs/rank_league_season_job.rb
@@ -1,5 +1,7 @@
 class RankLeagueSeasonJob < ApplicationJob
   def perform(league_season)
+  	return if league_season.blank?
+
   	LeagueSeason.transaction do
   		Rails.logger.info { "RankLeagueSeasonJob #{league_season.id}" }
 
