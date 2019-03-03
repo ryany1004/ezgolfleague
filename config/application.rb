1cba30cded86113d7500575d81529f4d3254d29e
Hunter Hillegas <hunter@lastonepicked.com>
2019-03-03 08:38:33 -0800

    Update to 5.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module EzgolfRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    config.load_defaults 5.0
+    config.load_defaults 5.2
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/}")]
