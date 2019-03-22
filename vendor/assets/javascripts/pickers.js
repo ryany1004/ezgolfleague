f02cc8f7df9f028da8da2dd9e0312115d52e7e8c
Hunter Hillegas <hunter@lastonepicked.com>
2019-03-22 15:43:41 -0700

    revert turbolinks calendar change

--- vendor/assets/javascripts/pickers.js
+++ vendor/assets/javascripts/pickers.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('ready page:change', function() {
   $('.datetimepicker').datetimepicker({
     // put here your custom picker options, that should be applied for all pickers
 
