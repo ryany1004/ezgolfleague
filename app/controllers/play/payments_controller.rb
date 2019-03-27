6c501abf55f3e06d5f88d860ac5bf83cc543127a
Hunter Hillegas <hunter@lastonepicked.com>
2019-03-27 09:34:26 -0700

    extra space remoed

--- app/controllers/play/payments_controller.rb
+++ app/controllers/play/payments_controller.rb
@@ -74,8 +74,7 @@ class Play::PaymentsController < Play::BaseController
           	email_addresses,
           	"A user paid league dues",
           	{ league_name: league_season.league.name, season_name: league_season.complete_name, dues_paid: league_season.league.dues_for_user(current_user, true),
-          		user: { complete_name: current_user.complete_name, email: current_user.email, phone_number: current_user.phone_number} })
-          unless email_addresses.blank?
+          		user: { complete_name: current_user.complete_name, email: current_user.email, phone_number: current_user.phone_number} }) unless email_addresses.blank?
         end
 
         self.create_payment(amount, charge_description, charge.id, nil, nil, league_season) #league dues
