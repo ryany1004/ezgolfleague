a4b3acd205fff504421639d5c7d9d0852cec318f
Hunter Hillegas <hunter@lastonepicked.com>
2019-03-30 17:21:27 -0700

    Don’t attempt to calculate net scores if there is no handicap allowance

--- app/jobs/calculate_net_scores_job.rb
+++ app/jobs/calculate_net_scores_job.rb
@@ -4,24 +4,25 @@ class CalculateNetScoresJob < ApplicationJob
 
   	handicap_computer = stroke_play.handicap_computer
 		handicap_allowance = handicap_computer.handicap_allowance(user: scorecard.user)
-		return if handicap_allowance.blank?
 
 		scorecard.scores.includes(:course_hole).each do |score|
 			score.net_strokes = score.strokes
 			
-			handicap_allowance.each do |h|
-				if h[:course_hole] == score.course_hole
-					hole_net_score = score.strokes
+			if handicap_allowance.present?
+				handicap_allowance.each do |h|
+					if h[:course_hole] == score.course_hole
+						hole_net_score = score.strokes
 
-					if h[:strokes] != 0
-						hole_adjusted_score = score.strokes - h[:strokes]
+						if h[:strokes] != 0
+							hole_adjusted_score = score.strokes - h[:strokes]
 
-          	if hole_adjusted_score > 0
-          		hole_net_score = hole_adjusted_score
-          	end
+	          	if hole_adjusted_score > 0
+	          		hole_net_score = hole_adjusted_score
+	          	end
+						end
+
+	        	score.net_strokes = hole_net_score
 					end
-
-        	score.net_strokes = hole_net_score
 				end
 			end
 
