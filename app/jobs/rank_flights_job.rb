549b0653962bace50677fae21b7e4b5f487ad092
Hunter Hillegas <hunter@lastonepicked.com>
2019-04-06 11:57:36 -0700

    Remove un-needed transaction

--- app/jobs/rank_flights_job.rb
+++ app/jobs/rank_flights_job.rb
@@ -1,11 +1,9 @@
 class RankFlightsJob < ApplicationJob
   def perform(tournament_day)
     tournament_day.flights.each do |f|
-    	Flight.transaction do 
-    		Rails.logger.info { "RankFlightsJob #{f.id}" }
+  		Rails.logger.info { "RankFlightsJob #{f.id}" }
 
-    		Flights::RankPosition.compute_rank(f)
-    	end
+  		Flights::RankPosition.compute_rank(f)
     end
   end
 end
