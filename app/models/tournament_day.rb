74888db52e4de5503833bae146469aa8bc2a908c
Hunter Hillegas <hunter@lastonepicked.com>
2019-04-06 13:37:47 -0700

    Delete results when re-calculating

--- app/models/tournament_day.rb
+++ app/models/tournament_day.rb
@@ -281,8 +281,10 @@ class TournamentDay < ApplicationRecord
   	end
   end
 
-  def score_all_rules
+  def score_all_rules(delete_first: false)
     self.scoring_rules.each do |rule|
+    	rule.tournament_day_results.delete_all if delete_first
+
       rule.score
       rule.rank
     end
