aa1863cf869169d5941a275857371540f759f043
Hunter Hillegas <hunter@lastonepicked.com>
2019-04-06 13:52:12 -0700

    Add rule debug info

--- app/jobs/rank_flights_job.rb
+++ app/jobs/rank_flights_job.rb
@@ -2,7 +2,7 @@ class RankFlightsJob < ApplicationJob
   def perform(tournament_day)
     tournament_day.scoring_rules.each do |rule|
     	tournament_day.flights.each do |flight|
-    		Rails.logger.info { "RankFlightsJob #{flight.id}" }
+    		Rails.logger.info { "RankFlightsJob #{flight.id} #{rule.id}" }
 
     		Flights::RankPosition.compute_rank(flight: flight, scoring_rule: rule)
     	end
