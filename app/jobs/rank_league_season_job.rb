27d8a7d1b5330a0d29138f0a9f9a52dbe961e918
Hunter Hillegas <hunter@lastonepicked.com>
2019-04-06 11:58:03 -0700

    Remove un-needed transaction

--- app/jobs/rank_league_season_job.rb
+++ app/jobs/rank_league_season_job.rb
@@ -2,10 +2,8 @@ class RankLeagueSeasonJob < ApplicationJob
   def perform(league_season)
   	return if league_season.blank?
 
-  	LeagueSeason.transaction do
-  		Rails.logger.info { "Ranking Season: #{league_season.id}" }
+		Rails.logger.info { "Ranking Season: #{league_season.id}" }
 
-  		LeagueSeasonRankingGroups::RankPosition.compute_rank(league_season)
-  	end
+		LeagueSeasonRankingGroups::RankPosition.compute_rank(league_season)
   end
 end
