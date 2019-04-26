701078a03819c6474128d4cc51c7eefcfdd5997c
Hunter Hillegas <hunter@lastonepicked.com>
2019-04-26 13:07:02 -0700

    Formatting

--- app/jobs/rank_league_season_job.rb
+++ app/jobs/rank_league_season_job.rb
@@ -1,9 +1,9 @@
 class RankLeagueSeasonJob < ApplicationJob
   def perform(league_season)
-  	return if league_season.blank?
+    return if league_season.blank?
 
-		Rails.logger.info { "Ranking Season: #{league_season.id}" }
+    Rails.logger.info { "Ranking Season: #{league_season.id}" }
 
-		LeagueSeasonRankingGroups::RankPosition.compute_rank(league_season)
+    LeagueSeasonRankingGroups::RankPosition.compute_rank(league_season)
   end
 end
