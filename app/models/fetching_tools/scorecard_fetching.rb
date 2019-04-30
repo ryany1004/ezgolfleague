2e9c9a010e57c05ef59ff2dcd844c69458019fee
Hunter Hillegas <hunter@lastonepicked.com>
2019-04-30 13:58:01 -0700

    do not include nil scorecards in related

--- app/models/fetching_tools/scorecard_fetching.rb
+++ app/models/fetching_tools/scorecard_fetching.rb
@@ -4,13 +4,13 @@ module FetchingTools
       scorecard = Scorecard.includes(scores: [:course_hole], golf_outing: [:user]).find(scorecard_id)
       tournament_day = scorecard.golf_outing.tournament_group.tournament_day
       tournament = tournament_day.tournament
-      
+
       other_scorecards = []
       scorecards_to_update = []
 
       tournament_day.scoring_rules.each do |rule|
         if rule.show_other_scorecards?
-          other_scorecards = rule.related_scorecards_for_user(scorecard.golf_outing.user) if other_scorecards.count == 0
+          other_scorecards = rule.related_scorecards_for_user(scorecard.golf_outing.user) if other_scorecards.count.zero?
 
           other_scorecards.each do |o|
             if (o.id >= 0) && !(scorecards_to_update.include? o) # omit non-human scorecards
--- app/models/scoring_rules/scorecard_support/best_ball_scorecard_support.rb
+++ app/models/scoring_rules/scorecard_support/best_ball_scorecard_support.rb
@@ -1,24 +1,25 @@
 module BestBallScorecardSupport
-	def related_scorecards_for_user(user, only_human_scorecards = false)
-		other_scorecards = []
+  def related_scorecards_for_user(user, only_human_scorecards = false)
+  	other_scorecards = []
 
-		team = self.tournament_day.daily_team_for_player(user)
-		unless team.blank?
-		  team.users.each do |u|
-		    if u != user
-		      other_scorecards << self.tournament_day.primary_scorecard_for_user(u)
-		    end
-		  end
-		end
+  	team = tournament_day.daily_team_for_player(user)
+  	unless team.blank?
+  	  team.users.each do |u|
+  	    if u != user
+          user_scorecard = tournament_day.primary_scorecard_for_user(u)
+  	      other_scorecards << user_scorecard if user_scorecard.present?
+  	    end
+  	  end
+  	end
 
-		if !only_human_scorecards
-		  gross_best_ball_card = self.best_ball_scorecard_for_user_in_team(user, team, false)
-		  net_best_ball_card = self.best_ball_scorecard_for_user_in_team(user, team, true)
+  	if !only_human_scorecards
+  	  gross_best_ball_card = best_ball_scorecard_for_user_in_team(user, team, false)
+  	  net_best_ball_card = best_ball_scorecard_for_user_in_team(user, team, true)
 
-		  other_scorecards << net_best_ball_card
-		  other_scorecards << gross_best_ball_card
-		end
+  	  other_scorecards << net_best_ball_card if net_best_ball_card.present?
+  	  other_scorecards << gross_best_ball_card if gross_best_ball_card.present?
+  	end
 
-		other_scorecards
-	end
+  	other_scorecards
+  end
 end
