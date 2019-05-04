4d6de104eccfa6a8e356f3a1735c72de54dccec7
Hunter Hillegas <hunter@lastonepicked.com>
2019-05-04 16:26:50 -0700

    Reformat for linter

--- app/models/scoring_rules/gross_low_scoring_rule.rb
+++ app/models/scoring_rules/gross_low_scoring_rule.rb
@@ -1,13 +1,13 @@
-class GrossLowScoringRule < LowScoringRule	
-	def name
-		"Gross Low"
-	end
+class GrossLowScoringRule < LowScoringRule
+  def name
+  	"Gross Low"
+  end
 
-	def description
-		"This contest is automatically calculated by the system. This contest takes the lowest gross score as the winner."
-	end
+  def description
+  	"This contest is automatically calculated by the system. This contest takes the lowest gross score as the winner."
+  end
 
-	def use_net_score
-		false
-	end
+  def use_net_score
+  	false
+  end
 end
