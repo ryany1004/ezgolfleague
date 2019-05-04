7a3c7aab623006b6d60f1417c95287221ba37857
Hunter Hillegas <hunter@lastonepicked.com>
2019-05-04 11:51:28 -0700

    Linting flight and daily team

--- app/models/daily_team.rb
+++ app/models/daily_team.rb
@@ -2,49 +2,48 @@ class DailyTeam < ApplicationRecord
   include Servable
 
   belongs_to :tournament_group, touch: true
-  has_and_belongs_to_many :users
-  has_many :daily_teams, class_name: "DailyTeam", foreign_key: "parent_team_id"
-  belongs_to :parent_team, class_name: "DailyTeam", touch: true, optional: true
+  has_and_belongs_to_many :users, dependent: :nullify, inverse_of: :daily_teams
+  has_many :daily_teams, class_name: 'DailyTeam', foreign_key: 'parent_team_id', inverse_of: :parent_team, dependent: :nullify
+  belongs_to :parent_team, class_name: 'DailyTeam', touch: true, optional: true
 
   validate :players_are_valid, on: :update
   def players_are_valid
-    other_teams = self.tournament_day.daily_teams
+    other_teams = tournament_day.daily_teams
 
-    self.users.each do |u|
+    users.each do |u|
       other_teams.each do |other_team|
         if other_team != self
-          errors.add(:user_ids, "can't include a user that's already on another team") if other_team.users.include? u
+          errors.add(:user_ids, 'can\'t include a user that\'s already on another team') if other_team.users.include? u
         end
       end
     end
   end
 
   def team_number_label
-    "Pairing ##{self.team_number} For Group"
+    "Pairing ##{team_number} For Group"
   end
 
   def name
-    complete_name = ""
+    complete_name = ''
 
-    self.users.each do |u|
+    users.each do |u|
       complete_name += u.complete_name
 
-      complete_name += " / " unless self.users.last == u
+      complete_name += ' / ' unless users.last == u
     end
 
     complete_name
   end
 
   def short_name
-    complete_name = ""
+    complete_name = ''
 
-    self.users.each do |u|
-      complete_name += u.first_name + " " + u.last_name[0] + "."
+    users.each do |u|
+      complete_name += u.first_name + ' ' + u.last_name[0] + '.'
 
-      complete_name += " / " unless self.users.last == u
+      complete_name += ' / ' unless users.last == u
     end
 
     complete_name
   end
-
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,9 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :tournament_day, inverse_of: :flights, touch: true
   belongs_to :course_tee_box
   belongs_to :league_season_scoring_group, inverse_of: :flights, optional: true
-  has_many :payouts, -> { order(:sort_order, "amount DESC, points DESC") }, inverse_of: :flight, dependent: :destroy
-  has_many :payout_results, -> { order("amount DESC, points DESC") }, inverse_of: :flight, dependent: :destroy
-  has_many :tournament_day_results, -> { order("sort_rank") }, inverse_of: :flight, dependent: :destroy
+  has_many :payouts, -> { order(:sort_order, 'amount DESC, points DESC') }, inverse_of: :flight, dependent: :destroy
+  has_many :payout_results, -> { order('amount DESC, points DESC') }, inverse_of: :flight, dependent: :destroy
+  has_many :tournament_day_results, -> { order(:sort_rank) }, inverse_of: :flight, dependent: :destroy
   has_and_belongs_to_many :users, inverse_of: :flights
 
   validates :flight_number, presence: true
@@ -35,29 +35,29 @@ class Flight < ApplicationRecord
   validate :does_not_overlap
   def does_not_overlap
     if upper_bound.blank? || lower_bound.blank?
-      errors.add(:upper_bound, "cannot validate an empty value")
-      errors.add(:lower_bound, "cannot validate an empty value")
+      errors.add(:upper_bound, 'cannot validate an empty value')
+      errors.add(:lower_bound, 'cannot validate an empty value')
 
       return
     end
 
-    if upper_bound >= 0 and lower_bound >= 0 #special case for imported data
-      other_flights = self.tournament_day.flights.where("id != ?", self.id)
+    if upper_bound >= 0 && lower_bound >= 0 # special case for imported data
+      other_flights = tournament_day.flights.where('id != ?', id)
 
       other_flights.each do |f|
         if lower_bound.between?(f.lower_bound, f.upper_bound)
-          errors.add(:lower_bound, "can't be in inside the range of an existing flight for this tournament")
+          errors.add(:lower_bound, 'can\'t be in inside the range of an existing flight for this tournament')
         end
 
         if upper_bound.between?(f.lower_bound, f.upper_bound)
-          errors.add(:upper_bound, "can't be in inside the range of an existing flight for this tournament")
+          errors.add(:upper_bound, 'can\'t be in inside the range of an existing flight for this tournament')
         end
       end
     end
   end
 
   def display_name(long_flight_name = false)
-    if self.league_season_scoring_group.blank?
+    if league_season_scoring_group.blank?
       if long_flight_name
         "Flight #{flight_number}"
       else
@@ -69,28 +69,25 @@ class Flight < ApplicationRecord
   end
 
   def api_display_name
-    self.display_name(true)
+    display_name(true)
   end
 
   def flight_id
-    self.id
+    id
   end
 
   def scorecard_base_scoring_rule_tournament_day_results
-  	self.tournament_day_results.joins(:scoring_rule).where(scoring_rules: { primary_rule: true })
+    tournament_day_results.joins(:scoring_rule).where(scoring_rules: { primary_rule: true })
   end
 
   def players
-    self.tournament_day_results.where("rank > 0")
+    tournament_day_results.where('rank > 0')
   end
 
-  #JSON
-
-  def as_json(options={})
+  def as_json(options = {})
     super(
       only: [:flight_number],
-      :methods => [:display_name, :api_display_name, :players]
+      methods: [:display_name, :api_display_name, :players]
     )
   end
-
 end
