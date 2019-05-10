5948971752dc6c408a6669f158264a16954f378c
Hunter Hillegas <hunter@lastonepicked.com>
2019-05-10 12:31:02 -0700

    Wrap this work in a transaction

--- app/controllers/team_configurations_controller.rb
+++ app/controllers/team_configurations_controller.rb
@@ -2,36 +2,38 @@ class TeamConfigurationsController < BaseController
   before_action :fetch_tournament
 
   def update
-    # un-tether all outings
-    @existing_outings = @tournament_day.golf_outings
-    @existing_outings.each do |g|
-      g.update(tournament_group: nil)
-    end
+    Tournament.transaction do
+      # un-tether all outings
+      @existing_outings = @tournament_day.golf_outings
+      @existing_outings.each do |g|
+        g.update(tournament_group: nil)
+      end
 
-    @tournament_day.reload
+      @tournament_day.reload
 
-    # add the pairings to the tee groups
-    @tournament_day.league_season_team_tournament_day_matchups.each do |matchup|
-      next if matchup.teams.count.zero?
+      # add the pairings to the tee groups
+      @tournament_day.league_season_team_tournament_day_matchups.each do |matchup|
+        next if matchup.teams.count.zero?
 
-      matchup.pairings_by_handicap.each do |pairing|
-        group = @tournament_day.tournament_group_with_open_slots(pairing.count)
-        raise 'No groups available' if group.blank?
+        matchup.pairings_by_handicap.each do |pairing|
+          group = @tournament_day.tournament_group_with_open_slots(pairing.count)
+          raise 'No groups available' if group.blank?
 
-        pairing.each do |user|
-          existing_outing = existing_outing_for_user(user)
-          if existing_outing.present?
-            existing_outing.update(tournament_group: group)
-          else
-            @tournament_day.add_player_to_group(tournament_group: group, user: user)
+          pairing.each do |user|
+            existing_outing = existing_outing_for_user(user)
+            if existing_outing.present?
+              existing_outing.update(tournament_group: group)
+            else
+              @tournament_day.add_player_to_group(tournament_group: group, user: user)
+            end
           end
         end
       end
-    end
 
-    # clean up any still needed
-    @existing_outings.each do |o|
-      o.destroy if o.tournament_group.blank?
+      # clean up any still needed
+      @existing_outings.each do |o|
+        o.destroy if o.tournament_group.blank?
+      end
     end
 
     redirect_to league_tournament_day_players_path(@league, @tournament, @tournament_day)
