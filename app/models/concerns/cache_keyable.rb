2ff63e0a0ea5109412bde42949093b2b5ca9fd18
Hunter Hillegas <hunter@lastonepicked.com>
2019-05-23 14:48:44 -0700

    Cache keyupdates

--- app/models/concerns/cache_keyable.rb
+++ app/models/concerns/cache_keyable.rb
@@ -1,18 +1,18 @@
 module CacheKeyable
   extend ActiveSupport::Concern
 
-  def cache_key(type = nil)
-    key = "#{self.class.name}-#{self.id}-#{self.updated_at.to_i}"
+  def cache_key(type = nil) # NOTE: May want to retire this with Rails 6, or change the name
+    key = "#{self.class.name}-#{id}-#{updated_at.to_i}"
     key = "#{key}-#{type}" if type
 
     key
   end
 
   def relation_cache_key(relation, type = nil)
-  	max_updated_at = relation.maximum(:updated_at).try(:utc).try(:to_s, :number)
-  	key = "#{self.class.name}-#{relation.first.class.name}-#{self.id}-#{max_updated_at}"
-		key = "#{key}-#{type}" if type
+    max_updated_at = relation.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    key = "#{self.class.name}-#{relation.first.class.name}-#{id}-#{max_updated_at}"
+    key = "#{key}-#{type}" if type
 
-  	key
+    key
   end
 end
