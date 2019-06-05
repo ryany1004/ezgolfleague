c03262bc5b2ff0980721f0fd9a91f090934439ad
Hunter Hillegas <hunter@lastonepicked.com>
2019-06-05 16:04:22 -0700

    Fix for regression in checking DQ too often

--- app/models/scoring_rules/scoring_rule_scorecards/best_ball_scorecard.rb
+++ app/models/scoring_rules/scoring_rule_scorecards/best_ball_scorecard.rb
@@ -60,6 +60,7 @@ module ScoringRuleScorecards
       end
 
       new_scores = []
+      eligible_users = scoring_rule.users_eligible_for_payouts
 
       scoring_rule.tournament_day.scorecard_base_scoring_rule.course_holes.each do |hole|
         score = DerivedScorecardScore.new
@@ -74,7 +75,7 @@ module ScoringRuleScorecards
           comparable_net_scores = []
 
           users_to_compare.each do |user|
-            next unless scoring_rule.users_eligible_for_payouts.include? user # do not use if DQ
+            next unless eligible_users.include? user # do not use if DQ
 
             scorecard = scoring_rule.tournament_day.primary_scorecard_for_user(user)
             raw_score = 0
--- app/models/scoring_rules/stroke_play_scoring_rule.rb
+++ app/models/scoring_rules/stroke_play_scoring_rule.rb
@@ -1,39 +1,39 @@
 class StrokePlayScoringRule < ScoringRule
-	include ::StrokePlayScoringRuleSetup
-	include ::StrokePlayScorecardSupport
+  include ::StrokePlayScoringRuleSetup
+  include ::StrokePlayScorecardSupport
 
-	def name
-		"Individual Stroke Play"
-	end
+  def name
+    'Individual Stroke Play'
+  end
 
-	def description
-		"Traditional stroke play for individual players."
-	end
+  def description
+    'Traditional stroke play for individual players.'
+  end
 
-	def legacy_game_type_id
-		1
-	end
+  def legacy_game_type_id
+    1
+  end
 
-	def scoring_computer
-		ScoringComputer::StrokePlayScoringComputer.new(self)
-	end
+  def scoring_computer
+    ScoringComputer::StrokePlayScoringComputer.new(self)
+  end
 
-	def show_other_scorecards?
-		true
-	end
+  def show_other_scorecards?
+    true
+  end
 
-	# TODO: this doesn't use the superclass version - should it?
+  # TODO: this doesn't use the superclass version - should it?
   def users_eligible_for_payouts
     eligible_player_list = []
 
-    if self.tournament.tournament_days.count == 1
-      eligible_player_list = self.tournament.qualified_players
-    else #only players that play all days can win
-      self.tournament.qualified_players.each do |player|
+    if tournament.tournament_days.count == 1
+      eligible_player_list = tournament.qualified_players
+    else # only players that play all days can win
+      tournament.qualified_players.each do |player|
         player_played_all_days = true
 
-        self.tournament.tournament_days.each do |day|
-          player_played_all_days = false if !self.tournament.includes_player?(player, day)
+        tournament.tournament_days.each do |day|
+          player_played_all_days = false if !tournament.includes_player?(player, day)
         end
 
         eligible_player_list << player if player_played_all_days
