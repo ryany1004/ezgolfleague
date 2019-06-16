43b283324adaabc82019a8b543b88793f984a7fe
Hunter Hillegas <hunter@lastonepicked.com>
2019-06-16 10:59:35 -0700

    add stableford to app scorecard

--- app/models/scorecards/api/scorecard_api_stableford.rb
+++ app/models/scorecards/api/scorecard_api_stableford.rb
@@ -4,7 +4,7 @@ module Scorecards
       def additional_rows
         rows = []
 
-        stableford_scorecard = scoring_rule.stableford_scorecard_for_user(user: scorecard.golf_outing.user)
+        stableford_scorecard = tournament_day.scorecard_base_scoring_rule.stableford_scorecard_for_user(user: scorecard.golf_outing.user)
         rows << score_row_for_scorecard(stableford_scorecard, stableford_scorecard.name(true))
 
         rows
