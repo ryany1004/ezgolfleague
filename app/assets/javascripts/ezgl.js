1fec5e40ff43de10f10c2d2b70339b62f0d9b74e
Cody Coe <cody@coexist.media>
2019-06-22 22:37:15 -0700

    Fixing welcome modal and billing link

--- app/assets/javascripts/ezgl.js
+++ app/assets/javascripts/ezgl.js
@@ -27,6 +27,33 @@
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+  // Hash tracking for Bootstrap tabs
+  $(document).ready(() => {
+    let url = location.href.replace(/\/$/, "");
+   
+    if (location.hash) {
+      const hash = url.split("#");
+      $('.panel-side-text a[href="#'+hash[1]+'"]').tab("show");
+      url = location.href.replace(/\/#/, "#");
+      history.replaceState(null, null, url);
+      setTimeout(() => {
+        $(window).scrollTop(0);
+      }, 400);
+    } 
+     
+    $('a[data-toggle="tab"]').on("click", function() {
+      let newUrl;
+      const hash = $(this).attr("href");
+      if(hash == "#v-edit-profile-tab") {
+        newUrl = url.split("#")[0];
+      } else {
+        newUrl = url.split("#")[0] + hash;
+      }
+      newUrl += "/";
+      history.replaceState(null, null, newUrl);
+    });
+  });
   
   // Collapse Navbar
   $(document).ready(function(){
