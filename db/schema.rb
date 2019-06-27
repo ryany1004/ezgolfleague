13c2672a91e0b871fd7f15791e0d22d5e19f44bc
Hunter Hillegas <hunter@lastonepicked.com>
2019-06-26 17:20:04 -0700

    forward people to the beta server if flag is set

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_155813) do
+ActiveRecord::Schema.define(version: 2019_06_27_001157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -591,6 +591,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_155813) do
     t.datetime "deleted_at"
     t.string "country"
     t.boolean "supereditor", default: false
+    t.boolean "beta_server", default: false
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
