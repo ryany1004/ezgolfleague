26eb847815ec2b59e4972013a99766ee5903d076
Hunter Hillegas <hunter@lastonepicked.com>
2019-06-27 08:33:45 -0700

    Improve performance on GHIN updater by pre-fetching days

--- lib/importers/ghin_importer.rb
+++ lib/importers/ghin_importer.rb
@@ -2,29 +2,27 @@ require 'open-uri'
 
 module Importers
   class GHINImporter
-
     def self.import_for_all_users
-      User.where("ghin_number IS NOT NULL").where("ghin_number != ''").where("ghin_updated_at <= ?", 5.minutes.ago).includes(:tournaments).order(:ghin_updated_at).each do |u|
-        if u.tournaments.where("tournament_starts_at > ?", DateTime.now)
-	        Importers::GHINImporter.import_ghin_for_user(u) 
+      User.where('ghin_number IS NOT NULL').where("ghin_number != ''").where('ghin_updated_at <= ?', 5.minutes.ago).includes(:tournaments).order(:ghin_updated_at).each do |u|
+        if u.tournaments.where('tournament_starts_at > ?', Time.zone.now)
+          Importers::GHINImporter.import_ghin_for_user(u)
 
-	        sleep 1
+          sleep 1
         end
       end
     end
 
     def self.recalc_course_handicap_for_user(user)
-      tournaments = Tournament.tournaments_happening_at_some_point(nil, nil, user.leagues, true).where(is_finalized: false)
+      tournaments = Tournament.tournaments_happening_at_some_point(nil, nil, user.leagues, true).where(is_finalized: false).includes(:tournament_days)
       tournaments.each do |t|
         t.tournament_days.each do |td|
           scorecard = td.primary_scorecard_for_user(user)
 
-          unless scorecard.blank?
-            puts "Updating Scorecard #{scorecard.id} Course Handicap for #{user.complete_name}"
+          if scorecard.present?
+            Rails.logger.info { "Updating Scorecard #{scorecard.id} Course Handicap for #{user.complete_name}" }
 
-            scorecard.set_course_handicap(true) #re-calc the course handicap
-
-            td.touch
+            scorecard.set_course_handicap(true)
+            td.touch # explicit touch needed because updating the course handicap skips callbacks
           end
         end
       end
@@ -33,43 +31,40 @@ module Importers
     def self.import_ghin_for_user(user)
       return nil if user.blank? || user.ghin_number.blank?
 
-      user_failure_string = "NOT Updating for #{user.complete_name} / #{user.id}: "
-
       begin
         url = user.ghin_url
 
-        puts "Reading: #{url}"
+        Rails.logger.info { "Reading: #{url}" }
         doc = Nokogiri::HTML(open(url))
 
-        unless doc.blank?
+        if doc.present?
           root_node = doc.search("//td[@class='ClubGridHandicapIndex']")
 
-          unless root_node.blank?
+          if root_node.present?
             handicap_index = root_node.children.last.children.to_s.to_f
 
-            unless handicap_index.blank?
-              puts "Handicap Index: #{handicap_index} for #{user.complete_name}"
+            if handicap_index.present?
+              Rails.logger.info { "Handicap Index: #{handicap_index} for #{user.complete_name}" }
 
-              unless handicap_index == 0.0
+              unless handicap_index.zero?
                 user.handicap_index = handicap_index
-                user.ghin_updated_at = DateTime.now
+                user.ghin_updated_at = Time.zone.now
                 user.save
 
                 Importers::GHINImporter.recalc_course_handicap_for_user(user)
               else
-                puts user_failure_string + "Not Updating - Zero Value"
+                Rails.logger.info { 'Not Updating - Zero Value' }
               end
             end
           else
-            puts user_failure_string + "Root node was blank"
+            Rails.logger.info { 'Root node was blank' }
           end
         else
-          puts user_failure_string + "Doc was blank"
+          Rails.logger.info { 'Doc was blank' }
         end
       rescue => e
-        puts user_failure_string + "GHIN Exception: #{e}"
+        Rails.logger.info { "GHIN Exception: #{e}" }
       end
     end
-
   end
 end
