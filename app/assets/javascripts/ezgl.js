52634d525121f131d1733209d6b2756f3844a7a5
Cody Coe <cody@coexist.media>
2019-06-30 01:25:39 -0700

    Updating tournaments show page

--- app/assets/javascripts/ezgl.js
+++ app/assets/javascripts/ezgl.js
@@ -54,6 +54,29 @@
       history.replaceState(null, null, newUrl);
     });
   });
+
+  $(document).ready(function(){
+    $( ".sortable" ).sortable({
+      connectWith: ".sortable",
+      zIndex: 9999999999999	
+    });
+
+    $('.popup-with-zoom-anim').magnificPopup({
+      type: 'inline',
+
+      fixedContentPos: false,
+      fixedBgPos: true,
+
+      overflowY: 'auto',
+
+      closeBtnInside: true,
+      preloader: false,
+      
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in'
+    });
+  })
   
   // Collapse Navbar
   $(document).ready(function(){
