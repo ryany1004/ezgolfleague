7b1cd038054d41cf43372f18e889e7d6ec4802ec
Hunter Hillegas <hunter@lastonepicked.com>
2019-07-30 17:53:59 -0700

    calculate all scoring rules on each scoring pass

--- lib/notifications/notification_strings.rb
+++ lib/notifications/notification_strings.rb
@@ -1,21 +1,19 @@
 module Notifications
-	class NotificationStrings
-	
-		def self.first_time_finalize(tournament_name = "your tournament")
-			return "Final results are now available for #{tournament_name}."
-		end
+  class NotificationStrings
+    def self.first_time_finalize(tournament_name = 'your tournament')
+      "#{tournament_name} - results have been posted."
+    end
 
-		def self.update_finalize(tournament_name = "your tournament")
-			return "Final results have been updated for #{tournament_name}."
-		end
+    def self.update_finalize(tournament_name = 'your tournament')
+      "#{tournament_name} - results have been updated."
+    end
 
-		def self.hole_in_one(username, hole_number)
-			return "#{username} just shot a hole-in-one on hole #{hole_number}!"
-		end
+    def self.hole_in_one(username, hole_number)
+      "#{username} just shot a hole-in-one on hole #{hole_number}!"
+    end
 
-		def self.birdie(username, hole_number)
-			return "#{username} just shot a birdie on hole #{hole_number}."
-		end
-
-	end
+    def self.birdie(username, hole_number)
+      "#{username} just shot a birdie on hole #{hole_number}."
+    end
+  end
 end
