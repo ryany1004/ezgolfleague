006d233d3e7d9a63963173d0696c58f8ffbedda3
Hunter Hillegas <hunter@lastonepicked.com>
2019-07-31 15:41:56 -0700

    Load Scorecards via AJAX

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -13,9 +13,13 @@ class ScorecardsController < BaseController
   end
 
   def show
-    eager_groups = fetch_eager_groups
+    # eager_groups = fetch_eager_groups
 
-    @next_scorecard = find_next_scorecard(@tournament_day, eager_groups, @scorecard)
+    # @next_scorecard = find_next_scorecard(@tournament_day, eager_groups, @scorecard)
+
+    respond_to do |format|
+      format.js { render layout: false }
+    end
   end
 
   def edit
