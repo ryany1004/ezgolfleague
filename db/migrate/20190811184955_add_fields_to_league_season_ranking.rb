120c48a1f5d2042e54488d03b141e4f64d35ad99
Hunter Hillegas <hunter@lastonepicked.com>
2019-08-13 15:23:49 -0700

    We can just set old leagues to a non zero average value and not have to re-calculate all of them

--- db/migrate/20190811184955_add_fields_to_league_season_ranking.rb
+++ db/migrate/20190811184955_add_fields_to_league_season_ranking.rb
@@ -4,15 +4,11 @@ class AddFieldsToLeagueSeasonRanking < ActiveRecord::Migration[5.2]
     add_column :league_seasons, :rankings_by_scoring_average, :boolean, default: false
 
     LeagueSeasonRanking.all.each do |r|
-      r.update(average_score: 0)
+      r.update(average_score: 1)
     end
 
     LeagueSeason.all.order(created_at: :desc).each do |l|
       l.update(rankings_by_scoring_average: false)
-    end
-
-    LeagueSeason.all.order(created_at: :desc).each do |l|
-      RankLeagueSeasonJob.perform_later(l)
     end
   end
 end
