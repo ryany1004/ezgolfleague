b8ae99c8302b91ac270fe7ff8236864e3ecffde2
Hunter Hillegas <hunter@lastonepicked.com>
2019-08-14 10:31:56 -0700

    Only validate new handicap bounds on create

--- app/models/league.rb
+++ app/models/league.rb
@@ -13,7 +13,7 @@ class League < ApplicationRecord
   validates :number_of_rounds_to_handicap, presence: true
   validates :number_of_lowest_rounds_to_handicap, presence: true
 
-  validate :handicap_rounds_bounds_are_correct
+  validate :handicap_rounds_bounds_are_correct, on: :create
   def handicap_rounds_bounds_are_correct
     if number_of_lowest_rounds_to_handicap > number_of_rounds_to_handicap
       errors.add(:number_of_lowest_rounds_to_handicap, 'cannot be more than the total number of rounds')
