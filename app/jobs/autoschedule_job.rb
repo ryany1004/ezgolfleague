974630a870440a1a0e51645fc053c11bc1f49f89
Hunter Hillegas <hunter@lastonepicked.com>
2019-08-24 09:30:31 -0700

    AutoSchedule debug

--- app/jobs/autoschedule_job.rb
+++ app/jobs/autoschedule_job.rb
@@ -1,6 +1,6 @@
 class AutoscheduleJob < ApplicationJob
   def perform(tournament_day)
-  	Rails.logger.info { "AutoscheduleJob Starting" }
+    Rails.logger.info { "AutoscheduleJob Starting for Day #{tournament_day.id}" }
 
     tournament_day.schedule_golfers
   end
