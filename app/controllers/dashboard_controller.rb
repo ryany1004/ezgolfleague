1884d4b8784cad66e17967b063cfbeb041f66fd0
Cody Coe <cody@coexist.media>
2019-09-29 19:47:04 -0700

    update: dashboard controller to pull next unfinalized tourney

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < BaseController
   def index
-    @next_tournament = Tournament.all_upcoming([current_user.selected_league]).limit(1).first
+    @next_tournament = Tournament.next_unfinalized([current_user.selected_league])
     @previous_tournament = Tournament.all_past([current_user.selected_league]).reorder(tournament_starts_at: :desc).limit(1).first
 
     @league_season = current_user.active_league_season
