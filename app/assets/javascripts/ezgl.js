a2ffac15d9d4ea8fd73e55c1ece0b7f65d2461de
Cody Coe <cody@coexist.media>
2019-10-13 23:40:52 -0700

    update: tee time section, popup

--- app/assets/javascripts/ezgl.js
+++ app/assets/javascripts/ezgl.js
@@ -3,49 +3,57 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 48
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
-  $(window).on("load",function(){
+  $(window).on("load", function() {
     $(".content").mCustomScrollbar();
   });
 
-  $(function () {
+  $(function() {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
   // Hash tracking for Bootstrap tabs
   $(document).ready(() => {
     let url = location.href.replace(/\/$/, "");
-   
+
     if (location.hash) {
       const hash = url.split("#");
-      $('.panel-side-text a[href="#'+hash[1]+'"]').tab("show");
+      $('.panel-side-text a[href="#' + hash[1] + '"]').tab("show");
       url = location.href.replace(/\/#/, "#");
       history.replaceState(null, null, url);
       setTimeout(() => {
         $(window).scrollTop(0);
       }, 400);
-    } 
-     
+    }
+
     $('a[data-toggle="tab"]').on("click", function() {
       let newUrl;
       const hash = $(this).attr("href");
-      if(hash == "#v-edit-profile-tab") {
+      if (hash == "#v-edit-profile-tab") {
         newUrl = url.split("#")[0];
       } else {
         newUrl = url.split("#")[0] + hash;
@@ -55,31 +63,31 @@
     });
   });
 
-  $(document).ready(function(){
-    $( ".sortable" ).sortable({
+  $(document).ready(function() {
+    $(".sortable").sortable({
       connectWith: ".sortable",
-      zIndex: 9999999999999	
+      zIndex: 9999999999999
     });
 
-    $('.popup-with-zoom-anim').magnificPopup({
-      type: 'inline',
+    $(".popup-with-zoom-anim").magnificPopup({
+      type: "inline",
 
-      fixedContentPos: false,
+      fixedContentPos: true,
       fixedBgPos: true,
 
-      overflowY: 'auto',
+      overflowY: "auto",
 
       closeBtnInside: true,
       preloader: false,
-      
+
       midClick: true,
       removalDelay: 300,
-      mainClass: 'my-mfp-zoom-in'
+      mainClass: "my-mfp-zoom-in"
     });
-  })
-  
+  });
+
   // Collapse Navbar
-  $(document).ready(function(){
+  $(document).ready(function() {
     var navbarCollapse = function() {
       if ($("#mainNav").offset().top > 50) {
         $("#mainNav").addClass("navbar-shrink");
