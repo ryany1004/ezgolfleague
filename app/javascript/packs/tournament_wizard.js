259570fe25d78abf5760454e879334290f17305d
Hunter Hillegas <hunter@lastonepicked.com>
2019-11-03 14:49:11 -0800

    Tournament Wizard stubs

---
+++ app/javascript/packs/tournament_wizard.js
@@ -0,0 +1,11 @@
+/* eslint no-console:0 */
+
+import Vue from 'vue/dist/vue.esm.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#tournament-wizard'
+  })
+
+  console.log(app)
+})
