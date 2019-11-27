0893bcd1ac3f723c4631be4929b18de51667219a
Hunter Hillegas <hunter@lastonepicked.com>
2019-11-27 09:45:23 -0800

    API for getting tournament groups

--- app/controllers/api/v2/tournament_groups_controller.rb
+++ app/controllers/api/v2/tournament_groups_controller.rb
@@ -1,2 +1,17 @@
 class Api::V2::TournamentGroupsController < BaseController
+  before_action :fetch
+
+  def index
+    @tournament_groups = @tournament_day.tournament_groups
+
+    registered_players = @tournament.players_for_day(@tournament.first_day)
+    @non_registered_players = @tournament.league.users.reject { |x| registered_players.include?(x) }
+  end
+
+  private
+
+  def fetch
+    @tournament = fetch_tournament_from_user_for_tournament_id(params[:tournament_id])
+    @tournament_day = @tournament.tournament_days.find(params[:tournament_day_id])
+  end
 end
