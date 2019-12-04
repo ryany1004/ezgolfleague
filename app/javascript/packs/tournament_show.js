5135354649b6621e3b009147167bea92fa477863
Hunter Hillegas <hunter@lastonepicked.com>
2019-12-04 12:26:02 -0800

    Linted tournament_show app

--- app/javascript/packs/tournament_show.js
+++ app/javascript/packs/tournament_show.js
@@ -1,4 +1,5 @@
-import Vue from "vue/dist/vue.esm.js";
+import Vue from 'vue/dist/vue.esm';
+
 import VModal from 'vue-js-modal';
 
 import { BSpinner } from 'bootstrap-vue';
@@ -6,44 +7,34 @@ import { BSpinner } from 'bootstrap-vue';
 import api from 'api';
 
 import Scorecard from '../components/Scorecard/Scorecard';
+import TeeTimeEditor from '../components/TeeTimes/TeeTimeEditor.vue';
 
 Vue.component('b-spinner', BSpinner);
-Vue.use(VModal, { componentName: "vue-modal" });
+Vue.use(VModal, { componentName: 'vue-modal' });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const anchorElement = document.getElementById("tournament-show");
-  const props = JSON.parse(anchorElement.getAttribute("data"));
-  const csrfToken = document
-    .querySelector('meta[name="csrf-token"]')
-    .getAttribute("content");
+document.addEventListener('DOMContentLoaded', () => {
+  const anchorElement = document.getElementById('tournament-show');
+  const props = JSON.parse(anchorElement.getAttribute('data'));
 
   const app = new Vue({
-    el: "#tournament-show",
+    el: '#tournament-show',
     components: {
       VModal,
       Scorecard,
+      TeeTimeEditor,
     },
     data: {
       csrfToken: document
         .querySelector('meta[name="csrf-token"]')
-        .getAttribute("content"),
+        .getAttribute('content'),
       teeTime: {},
     },
-    validations: {},
-    created: {},
-    mounted: {},
-    computed: {},
     methods: {
-      showTeeTimeEditorModal(addId) {
-        this.$modal.show("tee-time-editor");
-        const clickLink = "#grp-" + addId;
-
-        setTimeout(function() {
-          document
-            .getElementById("listScrollLink")
-            .setAttribute("href", clickLink);
-          document.getElementById("listScrollLink").click();
-        }, 100);
+      showTeeTimeEditor() {
+        api.getTournametGroups(props.league.id, props.tournament.id, props.tournament_day.id)
+          .then((response) => {
+            app.$modal.show('tee-time-editor', { teeGroupData: response.data });
+          });
       },
       displayScorecard(event) {
         app.$modal.show('scorecard-loading-modal');
